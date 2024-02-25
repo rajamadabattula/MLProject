7c856af75596d409a697753d391f8fc0b8e73d33
Raja Sekhar <rajamadabattula@gmail.com>
2024-02-25 14:03:23 -0700

    Added custom exceptions and logger

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_File = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(),"logs",LOG_File)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_File_PATH = os.path.join(logs_path,LOG_File)
+
+logging.basicConfig(
+    filename=LOG_File_PATH,
+    level=logging.INFO,
+
+)
+
+if __name__=="__main__":
+    logging.info("Logging has started")
